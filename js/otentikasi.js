ecdab7ec537ed1bbf9b4e9fdc3d992e671b0f619
Ridho Hafidz <mridhohafidz12@gmail.com>
2021-01-08 15:16:20 +0700

    Update

--- js/otentikasi.js
+++ js/otentikasi.js
@@ -2,7 +2,7 @@ function validate() {
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     if (email == "admin@siloker.com" && password == "12345") {
-        window.location.href = "index.html";
+        window.location.href = "halaman_admin.html";
         console.log("Login Sukses");
     } 
 
